59dcf7ee95df1d7f355599e31f0706ef7731540b
Bill Skrzypczak <bill@webbuildersconsulting.com>
2016-07-08 10:33:19 -0500

    CD

---
+++ Contents.swift
@@ -0,0 +1,39 @@
+
+import UIKit
+
+var str = "A Basic Class"
+
+// A Basic Class Definition Example
+//
+
+
+class Person                   // Give it a descriptive name
+{
+    var name: String = "Name"  // Define your attributes
+    var age:Int = 0            // Initialize them
+    
+    func profile() -> String { // Define your behaviours
+        
+        return "I'm \(self.name) and my mind feels \(self.age) years old." }
+    
+}
+
+
+
+// Once you define your Class you instantiate it to use it !!!!
+
+
+
+var p = Person()                // Instantiate an object
+
+
+
+p.name                         // Access a Class Property
+
+
+p.name = "Bill"                // Change a Class Property
+p.age = 25
+
+
+print(p.profile())             // Access a Class Method
+
